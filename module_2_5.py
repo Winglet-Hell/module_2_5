551e1036abf83572e6d086ef5e22588ba673d2f7
Winglet-Hell <i@stpra.ru>
2024-08-11 23:43:26 +0300

    Initial commit

--- module_2_5.py
+++ module_2_5.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+def get_matrix(n, m, value):
+    # Создаем пустую матрицу
+    matrix = []
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    # Внешний цикл для добавления строк
+    for _ in range(n):
+        # Создаем строку матрицы
+        row = []
+
+        # Внутренний цикл для добавления значений в строку
+        for _ in range(m):
+            row.append(value)
+
+        # Добавляем строку в матрицу
+        matrix.append(row)
+
+    # Возвращаем готовую матрицу
+    return matrix
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Примеры использования функции
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+result4 = get_matrix(5, 2, 555)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Вывод результата
+print(result1)
+print(result2)
+print(result3)
+print(result4)
